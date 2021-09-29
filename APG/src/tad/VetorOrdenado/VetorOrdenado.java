64542aed591ec16b87de7d9225d17abc478de22c
Júlio Silveira <81534830+JulioSilveiraFTTB@users.noreply.github.com>
2021-09-29 00:25:04 -0300

    Update VetorOrdenado.java
    o noob aq n entendeu o Map e n soube montar, ainda falta um método ou outro e adaptar tudo pra interface

---
+++ APG/src/tad/VetorOrdenado/VetorOrdenado.java
@@ -0,0 +1,57 @@
+package tad.VetorOrdenado;
+import utils.Estudante;
+
+    public class VetorOrdenado{
+
+        private Estudante[] estudantes = new Estudante[100000];
+
+        //guarda a quantidade de alunos existentes 
+        private int totalDeEstudantes = 0;
+
+        public void adiciona(Estudante estudante){
+            for(int i=0; i<this.estudantes.length; i++){
+                this.estudantes[this.totalDeEstudantes] = estudante;
+                this.totalDeEstudantes++;
+                System.out.println(this.estudantes[i]);
+            }
+            
+        }
+
+
+        private boolean posicaoOcupada(int posicao) {
+            return posicao >= 0 && posicao < this.totalDeEstudantes;
+        }
+
+
+        public void removeMatricula(int posicao) {
+            if (!this.posicaoOcupada(posicao)) {
+                throw new IllegalArgumentException("Posicao inválida");
+            }
+
+            for (int i = posicao; i < this.totalDeEstudantes - 1; i++) {
+                if(estudantes[i].getMatricula() <= 202060000){
+                    this.estudantes[i] = this.estudantes[i + 1];
+                }
+                    this.totalDeEstudantes--;
+                    System.out.println(estudantes);
+            }
+        }
+
+        public void estudantesES(int posicao){
+            int quantidade = 0;
+
+            for(int i = posicao; i < this.totalDeEstudantes - 1; i++){
+                if(estudantes[i].getCurso().equals("Engenharia de Software"));{
+                    quantidade++;
+                }
+                    System.out.println(quantidade);
+            }
+        }
+
+
+        public int tamanho(){
+            return this.totalDeEstudantes;
+        }
+    
+
+    }
